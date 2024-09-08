98e7e2783771a3de6196efa55e11c1a7229b59fa
Ben Muthalaly <benmuthalaly@gmail.com>
2024-09-08 04:46:15 -0500

    Make paddles respond to user input

--- src/game.zig
+++ src/game.zig
@@ -3,6 +3,8 @@ const rl = @cImport({
     @cInclude("raylib.h");
 });
 
+const movement_speed = 5;
+
 const Game = struct {
     allocator: std.mem.Allocator,
 
@@ -29,8 +31,6 @@ export fn init(window_width: c_int, window_height: c_int, paddle_height: c_int) 
     const game_state = allocator.create(Game) catch @panic("out of memory.");
 
     const paddle_width = @divTrunc(window_width, 10);
-    // const p1_x = ;
-    // const p2_x = window_width - paddle_width;
 
     game_state.* = .{
         .allocator = allocator,
@@ -40,8 +40,6 @@ export fn init(window_width: c_int, window_height: c_int, paddle_height: c_int) 
         .paddle_width = paddle_width,
         .p1_x = paddle_width,
         .p2_x = window_width - (2 * paddle_width),
-        // .p1_y = @divTrunc(window_height, 2) - @divTrunc(paddle_height, 2),
-        // .p2_y = @divTrunc(window_height, 2) - @divTrunc(paddle_height, 2),
         .p1_y = 0,
         .p2_y = window_height - paddle_height,
         .p1_dir = -1,
@@ -77,43 +75,31 @@ export fn update(game_state_ptr: *anyopaque) void {
     const p2_upper_bound = game_state.p2_y;
     const p2_lower_bound = game_state.p2_y + game_state.paddle_height;
 
-    if (p1_upper_bound <= 0 or p1_lower_bound >= game_state.window_height) game_state.p1_dir *= -1;
+    if (rl.IsKeyDown(rl.KEY_W)) {
+        if (p1_upper_bound > 0) {
+            game_state.p1_y -= movement_speed;
+        } else {
+            game_state.p1_y = 0;
+        }
+    } else if (rl.IsKeyDown(rl.KEY_S)) {
+        if (p1_lower_bound < game_state.window_height) {
+            game_state.p1_y += movement_speed;
+        } else {
+            game_state.p1_y = game_state.window_height - game_state.paddle_height;
+        }
+    }
 
-    if (p2_upper_bound <= 0 or p2_lower_bound >= game_state.window_height) game_state.p2_dir *= -1;
-
-    if (game_state.p1_dir >= 0) game_state.p1_y += 5;
-    if (game_state.p1_dir < 0) game_state.p1_y -= 5;
-
-    if (game_state.p2_dir >= 0) game_state.p2_y += 5;
-    if (game_state.p2_dir < 0) game_state.p2_y -= 5;
-
-    // game_state.mouse_on_text = rl.CheckCollisionPointRec(rl.GetMousePosition(), game_state.text_box);
-
-    // if (game_state.mouse_on_text) {
-    //     rl.SetMouseCursor(rl.MOUSE_CURSOR_IBEAM);
-
-    //     var key: u8 = @as(u8, @intCast(rl.GetCharPressed()));
-
-    //     while (key > 0) : (key = @as(u8, @intCast(rl.GetCharPressed()))) {
-    //         if ((key >= 32) and (key <= 125) and (game_state.letter_count < max_input_chars)) {
-    //             game_state.text[game_state.letter_count] = key;
-    //             game_state.letter_count += 1;
-    //             game_state.text[game_state.letter_count] = 0;
-    //         }
-    //     }
-
-    //     if (rl.IsKeyPressed(rl.KEY_BACKSPACE)) {
-    //         game_state.letter_count -|= 1;
-    //         // if (game_state.letter_count < 0) game_state.letter_count = 0;
-    //         game_state.text[game_state.letter_count] = 0;
-    //     }
-    // } else {
-    //     rl.SetMouseCursor(rl.MOUSE_CURSOR_DEFAULT);
-    // }
-
-    // if (game_state.mouse_on_text) {
-    //     game_state.frames_counter += 1;
-    // } else {
-    //     game_state.frames_counter = 0;
-    // }
+    if (rl.IsKeyDown(rl.KEY_UP)) {
+        if (p2_upper_bound > 0) {
+            game_state.p2_y -= movement_speed;
+        } else {
+            game_state.p2_y = 0;
+        }
+    } else if (rl.IsKeyDown(rl.KEY_DOWN)) {
+        if (p2_lower_bound < game_state.window_height) {
+            game_state.p2_y += movement_speed;
+        } else {
+            game_state.p2_y = game_state.window_height - game_state.paddle_height;
+        }
+    }
 }
