5bfe9398b227c0720d07f53d17fcea140d1caba5
Ben Muthalaly <benmuthalaly@gmail.com>
2024-09-08 04:00:35 -0500

    Make paddles move.

--- src/game.zig
+++ src/game.zig
@@ -19,6 +19,9 @@ const Game = struct {
 
     p2_x: c_int,
     p2_y: c_int,
+
+    p1_dir: c_int,
+    p2_dir: c_int,
 };
 
 export fn init(window_width: c_int, window_height: c_int, paddle_height: c_int) *anyopaque {
@@ -37,8 +40,12 @@ export fn init(window_width: c_int, window_height: c_int, paddle_height: c_int) 
         .paddle_width = paddle_width,
         .p1_x = paddle_width,
         .p2_x = window_width - (2 * paddle_width),
-        .p1_y = @divTrunc(window_height, 2) - @divTrunc(paddle_height, 2),
-        .p2_y = @divTrunc(window_height, 2) - @divTrunc(paddle_height, 2),
+        // .p1_y = @divTrunc(window_height, 2) - @divTrunc(paddle_height, 2),
+        // .p2_y = @divTrunc(window_height, 2) - @divTrunc(paddle_height, 2),
+        .p1_y = 0,
+        .p2_y = window_height - paddle_height,
+        .p1_dir = -1,
+        .p2_dir = 1,
     };
 
     return game_state;
@@ -55,15 +62,30 @@ export fn draw(game_state_ptr: *anyopaque) void {
     rl.BeginDrawing();
     rl.ClearBackground(rl.RAYWHITE);
 
-    rl.DrawRectangle(game_state.p1_x, game_state.p1_y, game_state.paddle_width, game_state.paddle_height, rl.Color{ .r = 0, .g = 0, .b = 0, .a = 200 });
-    rl.DrawRectangle(game_state.p2_x, game_state.p2_y, game_state.paddle_width, game_state.paddle_height, rl.Color{ .r = 0, .g = 0, .b = 0, .a = 200 });
+    rl.DrawRectangle(game_state.p1_x, game_state.p1_y, game_state.paddle_width, game_state.paddle_height, rl.Color{ .r = 0, .g = 0, .b = 0, .a = 255 });
+    rl.DrawRectangle(game_state.p2_x, game_state.p2_y, game_state.paddle_width, game_state.paddle_height, rl.Color{ .r = 0, .g = 0, .b = 0, .a = 255 });
 
     rl.EndDrawing();
 }
 
 export fn update(game_state_ptr: *anyopaque) void {
-    _ = game_state_ptr;
-    // const game_state: *game = @ptrCast(@alignCast(game_state_ptr));
+    const game_state: *Game = @ptrCast(@alignCast(game_state_ptr));
+
+    const p1_upper_bound = game_state.p1_y;
+    const p1_lower_bound = game_state.p1_y + game_state.paddle_height;
+
+    const p2_upper_bound = game_state.p2_y;
+    const p2_lower_bound = game_state.p2_y + game_state.paddle_height;
+
+    if (p1_upper_bound <= 0 or p1_lower_bound >= game_state.window_height) game_state.p1_dir *= -1;
+
+    if (p2_upper_bound <= 0 or p2_lower_bound >= game_state.window_height) game_state.p2_dir *= -1;
+
+    if (game_state.p1_dir >= 0) game_state.p1_y += 5;
+    if (game_state.p1_dir < 0) game_state.p1_y -= 5;
+
+    if (game_state.p2_dir >= 0) game_state.p2_y += 5;
+    if (game_state.p2_dir < 0) game_state.p2_y -= 5;
 
     // game_state.mouse_on_text = rl.CheckCollisionPointRec(rl.GetMousePosition(), game_state.text_box);
 
