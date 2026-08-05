2828301edb5e396cba5fe8c0a60b5362c864ee34
Ben Muthalaly <benmuthalaly@gmail.com>
2026-08-05 16:20:03 -0500

    Start hot reloading refactor.

--- src/raylib.zig
+++ src/raylib.zig
@@ -1,15 +1,17 @@
-//! followed https://zig.news/perky/hot-reloading-with-raylib-4bf9
+// followed https://zig.news/perky/hot-reloading-with-raylib-4bf9
 const std = @import("std");
 const rl = @import("raylib");
 const g = @import("game");
 
 // TODO
 // - watch the modified time on the configuration files inside main, if they've been modified, trigger a reload.
-// - recompile the DLL on a separate thread to avoid the game freeze.
+// - recompile the dylib on a separate thread to avoid the game freeze.
 //     - tweak build system:
-//         - write the editor DLL to a temporary file
-//         - unload, overwrite the DLL from the temporary one, and re-load.
+//         - write the editor dylib to a temporary file
+//         - unload, overwrite the dylib from the temporary one, and re-load.
 // - draw the output of the compilation on-screen, maybe in a custom debug window or in-editor console.
+
+const dylib_name = "zig-out/lib/libgame.0.0.1.dylib";
 
 fn rlColor(color: g.Color) rl.Color {
     return rl.Color{
@@ -22,7 +24,7 @@ fn rlColor(color: g.Color) rl.Color {
 
 const GamePtr = *anyopaque;
 
-// Import game types - we'll need to redeclare them to match the DLL
+// Import game types - we'll need to redeclare them to match the dylib
 const DrawState = extern struct {
     p1: rl.Rectangle,
     p2: rl.Rectangle,
@@ -38,20 +40,35 @@ const InputState = extern struct {
     p2_down: bool,
 };
 
-var initState: *const fn (u32, u32) callconv(.c) GamePtr = undefined;
-var deinit: *const fn (GamePtr) callconv(.c) void = undefined;
-var reload: *const fn (GamePtr) callconv(.c) void = undefined;
-var updateAndRender: *const fn (GamePtr, *const InputState) callconv(.c) void = undefined;
-var getDrawState: *const fn (GamePtr, *DrawState) callconv(.c) void = undefined;
+const GameCode = struct {
+    dyn_lib: ?std.DynLib = null,
+    dynlib_last_write_time: std.Io.Timestamp = .zero,
+
+    initState: ?*const fn (u32, u32) callconv(.c) GamePtr = null,
+    deinit: ?*const fn (GamePtr) callconv(.c) void = null,
+    reload: ?*const fn (GamePtr) callconv(.c) void = null,
+    updateAndRender: ?*const fn (GamePtr, ?*const InputState) callconv(.c) void = null,
+    getDrawState: ?*const fn (GamePtr, ?*DrawState) callconv(.c) void = null,
+
+    is_valid: bool = false,
+};
+
+var global_gc: GameCode = .{};
+
+// var initState: *const fn (u32, u32) callconv(.c) GamePtr = undefined;
+// var deinit: *const fn (GamePtr) callconv(.c) void = undefined;
+// var reload: *const fn (GamePtr) callconv(.c) void = undefined;
+// var updateAndRender: *const fn (GamePtr, *const InputState) callconv(.c) void = undefined;
+// var getDrawState: *const fn (GamePtr, *DrawState) callconv(.c) void = undefined;
 
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena;
     const allocator = arena.allocator();
-    loadGameDll() catch @panic("failed to load");
+    loadGameDynlib(dylib_name);
 
     const window_width = 800;
     const window_height = 450;
-    const game_state = initState(window_width, window_height);
+    const game_state = global_gc.initState.?(window_width, window_height);
     std.debug.print("Initial state: {any}\n", .{game_state});
 
     rl.setConfigFlags(.{ .window_resizable = true, .window_highdpi = true });
@@ -61,12 +78,14 @@ pub fn main(init: std.process.Init) !void {
     // WindowShouldClose will return true if the user presses ESC.
     while (!rl.windowShouldClose()) {
         if (rl.isKeyPressed(rl.KeyboardKey.slash)) {
-            unloadGameDll() catch unreachable;
-            recompileGameDll(init.io, allocator) catch {
+            unloadGameDynlib(&global_gc) catch unreachable;
+            recompileGameDynlib(init.io, allocator) catch {
                 std.debug.print("failed to recompile", .{});
             };
-            loadGameDll() catch @panic("failed to load");
-            reload(game_state);
+            loadGameDynlib(dylib_name);
+            if (global_gc.reload) |reload| {
+                reload(game_state);
+            }
         }
 
         // Collect input
@@ -78,12 +97,15 @@ pub fn main(init: std.process.Init) !void {
         };
 
         // Update game state
-        updateAndRender(game_state, &input);
+        if (global_gc.updateAndRender) |updateAndRender| {
+            updateAndRender(game_state, &input);
+        }
 
         // Get what to draw
         var draw_state: DrawState = undefined;
-        getDrawState(game_state, &draw_state);
-
+        if (global_gc.getDrawState) |getDrawState| {
+            getDrawState(game_state, &draw_state);
+        }
         // Render
         rl.beginDrawing();
         rl.clearBackground(rl.Color.white);
@@ -105,34 +127,54 @@ pub fn main(init: std.process.Init) !void {
     std.debug.print("Window closed, about to return from main\n", .{});
 }
 
-var editor_dyn_lib: ?std.DynLib = null;
-fn loadGameDll() !void {
-    if (editor_dyn_lib != null) return error.AlreadyLoaded;
-
-    var dyn_lib = std.DynLib.open("zig-out/lib/libgame.0.0.1.dylib") catch {
-        return error.OpenFail;
+fn loadGameDynlib(src_dylib_name: []const u8) void {
+    const dyn_lib: ?std.DynLib = std.DynLib.open(src_dylib_name) catch blk: {
+        global_gc.dyn_lib = null;
+        global_gc.initState = null;
+        global_gc.deinit = null;
+        global_gc.reload = null;
+        global_gc.updateAndRender = null;
+        global_gc.getDrawState = null;
+        global_gc.is_valid = false;
+        break :blk null;
     };
-    editor_dyn_lib = dyn_lib;
 
-    initState = dyn_lib.lookup(@TypeOf(initState), "initState") orelse return error.lookupFail;
-    deinit = dyn_lib.lookup(@TypeOf(deinit), "deinit") orelse return error.lookupFail;
-    reload = dyn_lib.lookup(@TypeOf(reload), "reload") orelse return error.lookupFail;
-    updateAndRender = dyn_lib.lookup(@TypeOf(updateAndRender), "updateAndRender") orelse return error.lookupFail;
-    getDrawState = dyn_lib.lookup(@TypeOf(getDrawState), "getDrawState") orelse return error.lookupFail;
+    global_gc.dyn_lib = dyn_lib;
 
+    if (global_gc.dyn_lib) |*dl| {
+        global_gc.initState = dl.lookup(@TypeOf(global_gc.initState.?), "initState");
+        global_gc.deinit = dl.lookup(@TypeOf(global_gc.deinit.?), "deinit");
+        global_gc.reload = dl.lookup(@TypeOf(global_gc.reload.?), "reload");
+        global_gc.updateAndRender = dl.lookup(@TypeOf(global_gc.updateAndRender.?), "updateAndRender");
+        global_gc.getDrawState = dl.lookup(@TypeOf(global_gc.getDrawState.?), "getDrawState");
+
+        global_gc.is_valid = global_gc.initState != null and
+            global_gc.deinit != null and
+            global_gc.reload != null and
+            global_gc.updateAndRender != null and
+            global_gc.getDrawState != null;
+    }
+
+    if (!global_gc.is_valid) {
+        global_gc.initState = null;
+        global_gc.deinit = null;
+        global_gc.reload = null;
+        global_gc.updateAndRender = null;
+        global_gc.getDrawState = null;
+    }
     std.debug.print("Loaded dll\n", .{});
 }
 
-fn unloadGameDll() !void {
-    if (editor_dyn_lib) |*dyn_lib| {
+fn unloadGameDynlib(gc: *GameCode) !void {
+    if (gc.dyn_lib) |*dyn_lib| {
         dyn_lib.close();
-        editor_dyn_lib = null;
+        gc.dyn_lib = null;
     } else {
         return error.AlreadyUnloaded;
     }
 }
 
-fn recompileGameDll(io: std.Io, allocator: std.mem.Allocator) !void {
+fn recompileGameDynlib(io: std.Io, allocator: std.mem.Allocator) !void {
     const process_args = [_][]const u8{
         "zig",
         "build",
